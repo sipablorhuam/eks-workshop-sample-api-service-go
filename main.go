5ba6c4aac95258c5ed261d5c203b10cb56a237ee
sipablorhuam <71754067@mackenzista.com.br>
2020-08-20 18:41:04 -0300

    Update main.go

--- main.go
+++ main.go
@@ -15,7 +15,7 @@ func main() {
 
 		f := fib()
 
-		res := &response{Message: "Hello Diego!"}
+		res := &response{Message: "Hello Pablo!"}
 
 		for _, e := range os.Environ() {
 			pair := strings.Split(e, "=")
